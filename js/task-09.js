4b2e789c1466411a22d87e00b77a244ddb5acdc2
Anton Litoshenko <99186249+AntonLitoshenko@users.noreply.github.com>
2023-03-20 04:47:40 +0200

    task -09 \ done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const btnColorChange = document.querySelector('.change-color');
+
+btnColorChange.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 }
