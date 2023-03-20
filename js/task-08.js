fdaa4ac28d0616fb5b354c225c4cbdd3b8aed784
Anton Litoshenko <99186249+AntonLitoshenko@users.noreply.github.com>
2023-03-20 04:32:54 +0200

    task-08 \ done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onLoginFormSub);
+
+function onLoginFormSub(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  console.log(formElements);
+  const email = formElements.email;
+  const password = formElements.password;
+  const formData = new FormData(event.currentTarget);
+  console.log(formData);
+
+  if (email.value === '' || password.value === '') {
+    alert('All fields must be filled');
+  } else {
+    alert('The form has been submitted!');
+  }
+}
