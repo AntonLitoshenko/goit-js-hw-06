5aa5ffcb91ccf9d5870a585610f2e12201c5a141
Anton Litoshenko <99186249+AntonLitoshenko@users.noreply.github.com>
2023-03-20 23:31:16 +0200

    task-09 \ done

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,22 @@
-const refs = {
-  input: document.querySelector('[id="name-input"]'),
-  output: document.querySelector('[id="name-output"]'),
-  spanEl: document.querySelector('span'),
-};
-refs.input.addEventListener('input', onInputChange);
+//const refs = {
+//input: document.querySelector('[id="name-input"]'),
+//output: document.querySelector('[id="name-output"]'),
+//spanEl: document.querySelector('span'),
+//};
+//refs.input.addEventListener('input', onInputChange);
+//function onInputChange(event) {
+//if (event.currentTarget.value.trim() !== '') {
+//refs.output.textContent = event.currentTarget.value;
+//}
+//}
+
+const input = document.getElementById('name-input');
+const output = document.getElementById('name-output');
+input.addEventListener('input', onInputChange);
 function onInputChange(event) {
   if (event.currentTarget.value.trim() !== '') {
-    refs.output.textContent = event.currentTarget.value;
+    output.textContent = event.currentTarget.value;
+  } else {
+    output.textContent = 'Anonymous';
   }
 }
